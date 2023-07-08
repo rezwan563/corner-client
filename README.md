a836427336c86372b8dcb85c5251b48fd6379a43
Rezwan <t.rewan17@gmail.com>
2023-07-08 20:29:08 +0600

    Created Corner project

---
+++ README.md
@@ -0,0 +1,7 @@
+## CORNER APP
+
+This is a learning project. I will try to implement all the essential things I have learnt over the last six months into this app. This is going to be social media app named CORNER. 
+
+### Note to Myself
+
+* Using eslintrc teaches you the best practices while developing react app. It can bother you with the red file names. However, if you hover over why eslintrc doing this it will also give you hint how to solve it. You should use rel='noreferrer' when use target="_blank" in anchor tag.
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHOMZ9CJ0YCe0wemDLi3TOJfCPs8tgIQk",
+  authDomain: "corner-app-dd8a2.firebaseapp.com",
+  projectId: "corner-app-dd8a2",
+  storageBucket: "corner-app-dd8a2.appspot.com",
+  messagingSenderId: "234322282425",
+  appId: "1:234322282425:web:8b7700fa2376f732369868"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
