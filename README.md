622bb4a8579bb42c37f1fd6addb828fe58e056b7
Rezwan <t.rewan17@gmail.com>
2023-07-10 01:31:27 +0600

    Created user post card

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This is a learning project. I will try to implement all the essential things I h
 
 ### Note to Myself
 
-* Using eslintrc teaches you the best practices while developing react app. It can bother you with the red file names. However, if you hover over why eslintrc doing this it will also give you hint how to solve it. You should use rel='noreferrer' when use target="_blank" in anchor tag.
+Using eslintrc teaches you the best practices while developing react app. It can bother you with the red file names. However, if you hover over why eslintrc doing this it will also give you hint how to solve it. You should use rel='noreferrer' when use target="_blank" in anchor tag.
+
+While building the user interface I can't get my head around how to store data in database. Currently, I am thinking about MongoDB for the database. I can understand I need to store the users data in a document. That should be easy. But, with one users you have their posts, likes, friends, their private messages and so on. You get the idea. So, how should you store those data so that you can easily access those data in future? I was googling and learned two new terms **Data Modeling** and **Application Design** sounds intimidating! 
--- src/pages/Homepage/HomeComponents/NewsFeed.jsx
+++ src/pages/Homepage/HomeComponents/NewsFeed.jsx
@@ -1,35 +1,60 @@
-import { BsThreeDots, BsShare, BsChat, BsBookmark, BsFillHeartFill, BsHeart } from "react-icons/bs";
+import { useState } from "react";
+import {
+  BsThreeDots,
+  BsShare,
+  BsChat,
+  BsBookmark,
+  BsFillHeartFill,
+  BsHeart,
+} from "react-icons/bs";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 const NewsFeed = () => {
+  const [showDetails, setShowDetails] = useState(false)
+  const [isLoved, setIsLoved] = useState(false)
   return (
     <div className="p-2  bg-white rounded-md">
       <div className="flex justify-between">
-        <div className="flex items-start gap-2">
+        <div className="flex items-start gap-2 ">
           <img
             src="https://i.ibb.co/64jgrXk/istockphoto-1335941248-612x612.jpg"
             alt=""
             className="w-9 h-9 rounded-full"
           />
+
           <div className="">
             <span className="font-bold">Rcyt87</span>
             <p className="text-xs">Dhaka, Bangladesh</p>
           </div>
         </div>
-        <BsThreeDots className="text-2xl cursor-pointer" />
+        <div className="relative">
+          <BsThreeDots onClick={() => setShowDetails(!showDetails)} className="text-2xl cursor-pointer" />
+          <div className={`w-36 absolute top-10 right-2 bg-white p-2 border rounded-md border-slate-300 z-10 ${showDetails ? '' : 'hidden'}`}>
+            <ul>
+              <div className="flex justify-between">
+                <li className="pb-2 cursor-pointer ">Report </li>
+
+                <IoIosCloseCircleOutline onClick ={() => setShowDetails(!showDetails)}className="text-3xl relative top-[-10px] right-[-10px] cursor-pointer"/>
+              </div>
+
+              <li className="pt-2 border-t-2 cursor-pointer ">Not Interested</li>
+            </ul>
+          </div>
+        </div>
       </div>
       <img
         src="https://i.ibb.co/rMGJPmD/maxresdefault.jpg"
         alt=""
-        className="py-2 w-fit h-"
+        className="py-2 max-w-full h-96"
       />
       <div className="flex justify-between ">
-        <div className="flex gap-2">
-          <BsFillHeartFill className="text-2xl" />
-          <BsHeart className="text-2xl"/>
-          <BsChat className="text-2xl"/>
-          <BsShare className="text-2xl"/>
+        <div className="flex gap-4">
+          <BsFillHeartFill onClick={() => setIsLoved(!isLoved)} className={`text-2xl cursor-pointer text-red-700 ${isLoved ? '': 'hidden'}`} />
+          <BsHeart onClick={() => setIsLoved(!isLoved)} className={`text-2xl cursor-pointer ${isLoved ? 'hidden' : ''}`} />
+          <BsChat className="text-2xl cursor-pointer" />
+          <BsShare className="text-2xl cursor-pointer" />
         </div>
-        <BsBookmark className="text-2xl" />
+        <BsBookmark className="text-2xl cursor-pointer" />
       </div>
     </div>
   );
